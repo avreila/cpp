7e3bc8ec78de0c70e9129fda9f0a3f8a47a99775
avreila <dile2897@mail.ru>
2024-10-23 17:55:28 +0500

    first version

---
+++ 3/3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using std::string;
+using std::cout;
+using std::cin;
+
+
+int main() {
+    setlocale(LC_ALL, "RU");
+  
+
+    return 0;
+
+}
